df121c81c9cf71b3061bd4393ba876250c5cc307
Lorenzo Pagani <larrypg2@gmail.com>
2024-01-19 18:50:37 +0100

    style: adds basic footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,24 @@
 @extends('app')
 
 @section('content')
-    <div class="jumbo">
-        <img class="w-100" src="assets\images\jumbotron.jpg" alt="jumbotron">
-    </div>
-    <div class="comics-wrapper">
-        <div class="container">
-            <div class="row justify-between">
-                @foreach ($comics as $comic)
-                    <div class="comic-card">
-                        <img class="w-100" src="{{ $comic['thumb'] }}" alt="">
-                        <h6>{{ $comic['title'] }}</h6>
-                    </div>
-                @endforeach
+    <main>
+        <div class="jumbo">
+            <img class="w-100" src="assets\images\jumbotron.jpg" alt="jumbotron">
+        </div>
+        <div class="comics-wrapper">
+            <div class="container">
+                <div class="row justify-between">
+                    @foreach ($comics as $comic)
+                        <div class="comic-card">
+                            <img class="w-100" src="{{ $comic['thumb'] }}" alt="">
+                            <h6>{{ $comic['title'] }}</h6>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
+    </main>
+    <footer>
+        @include('partials.footer')
+    </footer>
 @endsection
